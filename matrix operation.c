10a0aa2ff99bc19559941028e011893e391980a3
Abdullah Al Mahdi <abdullahalmahdi2006@gmail.com>
2025-05-24 14:02:40 +0600

    Update matrix operation.c

--- matrix operation.c
+++ matrix operation.c
@@ -3,9 +3,8 @@
 void add(int ra, int ca, int a[ra][ca], int b[ra][ca]);
 void sub(int ra, int ca, int a[ra][ca], int b[ra][ca]);
 void multi(int ra, int ca, int rb, int cb, int a[ra][ca], int b[rb][cb]);
-
 void transpose(int r, int c, int matrix[r][c]);
-void isSquareMatrix(int r, int c, char name);
+int determinant(int n, int mat[n][n]);
 
 int main() {
     int ra, ca, rb, cb;
@@ -44,9 +43,13 @@ int main() {
 
     printf("4. Transpose of A\n");
     printf("5. Transpose of B\n");
-    printf("6. Check if A is square\n");
-    printf("7. Check if B is square\n");
-    printf("8. Exit\n");
+    printf("6. Determinant of A\n");
+    printf("7. Determinant of B\n");
+    printf("8. Check if A is Identity matrix\n");
+    printf("9. Check if B is Identity matrix\n");
+    printf("10. Check if A is square\n");
+    printf("11. Check if B is square\n");
+    printf("12. Exit\n");
 
 
     while (1) {
@@ -89,14 +92,42 @@ int main() {
                 break;
 
             case 6:
+                if (ra == ca)
+                    printf("\nDeterminant of A: %d\n", determinant(ra, a));
+                else
+                    printf("\nDeterminant is only defined for square matrices.\n");
+                break;
+            case 7:
+                if (rb == cb)
+                    printf("\nDeterminant of B: %d\n", determinant(rb, b));
+                else
+                    printf("\nDeterminant is only defined for square matrices.\n");
+                break;
+            case 8:
+                if (ra == ca) {
+                isIdentityMatrix(ra, a);
+                }
+                else{
+                printf("\nIdentity check is only valid for square matrices.\n");
+                }
+                break;
+            case 9:
+                if (rb == cb) {
+                isIdentityMatrix(rb, b);
+                }
+                else{
+                printf("\nIdentity check is only valid for square matrices.\n");
+                }
+                break;
+            case 10:
                 isSquareMatrix(ra, ca, 'A');
                 break;
 
-            case 7:
+            case 11:
                 isSquareMatrix(rb, cb, 'B');
                 break;
 
-            case 8:
+            case 12:
                 printf("\nThanks for using the program.\n");
                 return 0;
 
@@ -158,14 +189,32 @@ void transpose(int r, int c, int matrix[r][c]) {
     }
 }
 
-void isSquareMatrix(int r, int c, char name) {
-    if (r == c)
-    {
-
-        printf("\nMatrix %c is a square matrix.\n", name);
+int determinant(int n, int mat[n][n]) {
+    if (n == 1){
+        return mat[0][0];
     }
-    else
-    {
-        printf("\nMatrix %c is not a square matrix.\n", name);
+    if (n == 2){
+        return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
     }
+    if (n == 3){
+        return mat[0][0]*(mat[1][1]*mat[2][2] - mat[1][2]*mat[2][1]) -
+               mat[0][1]*(mat[1][0]*mat[2][2] - mat[1][2]*mat[2][0]) +
+               mat[0][2]*(mat[1][0]*mat[2][1] - mat[1][1]*mat[2][0]);
+    }
+    else{
+    printf("Determinant calculation is only supported for 1x1, 2x2, and 3x3 matrices.\n");
+    }
+    return 0;
 }
+void isIdentityMatrix(int n, int mat[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((i == j && mat[i][j] != 1) || (i != j && mat[i][j] != 0)) {
+                printf("\nMatrix is NOT an identity matrix.\n");
+                return;
+            }
+        }
+    }
+    printf("\nMatrix IS an identity matrix.\n");
+}
+
